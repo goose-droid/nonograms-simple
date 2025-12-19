a673cf692ddc3b6d69ddaf848ec0475449ff419c
Oliver Christensen <ollie.a.c@outlook.com>
2025-12-19 13:53:47 -0700

    adds comments to javascript file

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
+//class for puzzles
 class Puzzle {
-
     solution = "0000000000000000000000000";
     hintValues = ["", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"]; 
     name = "default"
+    //array of puzzles, for easy selection
     static puzzles = [];
-
+    
     constructor(solution, hintValues, name) {
         this.solution = solution;
         this.hintValues = hintValues;
         this.name = name;
         Puzzle.puzzles.push(this);
-
     }
 }
 
+//constants from html
 const boxes = document.querySelectorAll(".puzzle div.grid");
 const hints = document.querySelectorAll(".puzzle div.border");
-//const solution = [true, true, true, false, true, false, false, true, false];
-//const hintValues = ["", "1", "3", "1", "3", "1", "1", "1", "1", "1", "1"];
-//const solution = "1110100101111111111100000";
-const buttonCheck = document.querySelector("button.check");
 const result = document.querySelector("#result");
-const buttonReset = document.querySelector("button.reset"); 
+const buttonCheck = document.querySelector("button.check");
+const buttonReset = document.querySelector("button.reset");
 
+//puzzles
 const circlePuzzle = new Puzzle("0000001110010100111000000", ["", "0", "3", "1<br>1", "3", "0", "0", "3", "1, 1", "3", "0"], "circle");
 const mugPuzzle = new Puzzle("0000011111111011110111111", ["", "4", "4", "4", "1<br>1", "4", "0", "5", "3, 1", "3, 1", "5"], "mug");
 const kiPuzzle = new Puzzle("0010011111001000111010101", ["", "1<br>1", "1<br>1", "5", "1<br>1", "1<br>1", "1", "5", "1", "3", "1, 1, 1"], "木 (tree)");
 const hiPuzzle = new Puzzle("0111101001011110100101111", ["", "0", "5", "1<br>1<br>1", "1<br>1<br>1", "5", "4", "1, 1", "4", "1, 1", "4"], "日 (sun)");
 
+//variables
 let currentPuzzle;
 let currentHintValues;
 let currentSolution;
 
-
+//functions
 function toggleBox(e) {
     e.target.classList.toggle("clicked");
 }
@@ -42,16 +42,10 @@ function getNewPuzzle() {
     currentPuzzle = Puzzle.puzzles[puzzlePicker];
     currentHintValues = currentPuzzle.hintValues;
     currentSolution = currentPuzzle.solution;
-        for (let i = 0; i < hints.length; i++) {
-            hints[i].innerHTML = currentHintValues[i];
-        }
+    for (let i = 0; i < hints.length; i++) {
+        hints[i].innerHTML = currentHintValues[i];
+    }
 }
-
-getNewPuzzle();
-
-boxes.forEach((box) => {
-    box.addEventListener("click", toggleBox);
-})
 
 function checkAnswer() {
     var answer = "";
@@ -90,6 +84,14 @@ function resetPuzzle () {
     })
     getNewPuzzle();
 }
+
+//main code
+
+getNewPuzzle();
+
+boxes.forEach((box) => {
+    box.addEventListener("click", toggleBox);
+})
 
 buttonCheck.addEventListener("click", checkAnswer);
 buttonReset.addEventListener("click", resetPuzzle);
