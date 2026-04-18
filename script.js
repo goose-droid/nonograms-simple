ac9e13c2ed84f53f0ccb382fa3e8d4ac5e33acd3
goose-droid <ollie.a.c@outlook.com>
2026-04-18 08:26:06 -0600

    Update script.js
    Fix random button

--- script.js
+++ script.js
@@ -211,4 +211,5 @@ boxes.forEach((box) => {
 buttonCheck.addEventListener("click", checkAnswer);
 buttonReset.addEventListener("click", resetPuzzle);
 buttonCatSubmit.addEventListener("click", clickCatSubmit);
+buttonRandom.addEventListener("click", resetPuzzle);
     
